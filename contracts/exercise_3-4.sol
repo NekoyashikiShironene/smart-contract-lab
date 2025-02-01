5dbcf0cb9cee7e09f63b26fbe2298fc494714cfd
NekoyashikiShironene <yanason.ys2016@gmail.com>
2025-02-01 18:15:45 +0700

    Fixed variable name

--- contracts/exercise_3-4.sol
+++ contracts/exercise_3-4.sol
@@ -4,15 +4,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Contract {
     function count_word(string memory text) public pure returns(uint256)  {
-        bytes memory textBytes = bytes(text);
-        if (textBytes.length == 0) {
+        bytes memory strBytes = bytes(text);
+        if (strBytes.length == 0) {
             return 0;
         }
-        
 
         uint256 wordCount = 1;
-        for (uint256 i = 0; i < textBytes.length; i++) {
-            if (textBytes[i] == 0x20) {
+        for (uint256 i = 0; i < strBytes.length; i++) {
+            if (strBytes[i] == 0x20) {
                 wordCount++;
             }
         }
@@ -21,19 +20,19 @@ contract Contract {
     }
 
     function split(string memory text) public pure returns(string[] memory) {
-        bytes memory strByte = bytes(text);
+        bytes memory strBytes = bytes(text);
         uint256 wordCount = count_word(text);
         string[] memory words = new string[](wordCount);
         
         uint256 wordIndex = 0;
         uint256 lastIndex = 0;
 
-        for (uint256 i = 0; i <= strByte.length; i++) {
-            if (i == strByte.length || strByte[i] == 0x20) {
+        for (uint256 i = 0; i <= strBytes.length; i++) {
+            if (i == strBytes.length || strBytes[i] == 0x20) {
                 uint256 wordLength = i - lastIndex;
                 bytes memory word = new bytes(wordLength);
                 for (uint256 j = 0; j < wordLength; j++) {
-                    word[j] = strByte[lastIndex + j];
+                    word[j] = strBytes[lastIndex + j];
                 }
                 words[wordIndex] = string(word);
                 wordIndex++;
