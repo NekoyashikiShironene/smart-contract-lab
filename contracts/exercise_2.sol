37a92f9ae0d8500e08beeddf1ba9e46935574908
NekoyashikiShironene <yanason.ys2016@gmail.com>
2025-02-02 20:36:49 +0700

    Optimize gas

--- contracts/exercise_2.sol
+++ contracts/exercise_2.sol
@@ -4,17 +4,17 @@ pragma solidity >=0.7.0 <0.9.0;
 
 
 contract CheckIn {
-    mapping(address => uint256) usersCheckedIn;
+    mapping(address => uint256) public usersCheckedIn;
     address[] users;
 
-    function check_in() public {
+    function check_in() external {
         if (usersCheckedIn[msg.sender] == 0) {
             users.push(msg.sender);
         }
         usersCheckedIn[msg.sender] += 1;
     }
 
-    function report() public view returns(address[] memory, uint256[] memory) {
+    function report() external view returns(address[] memory, uint256[] memory) {
         uint256 length = users.length;
         uint256[] memory counts = new uint256[](length);
 
