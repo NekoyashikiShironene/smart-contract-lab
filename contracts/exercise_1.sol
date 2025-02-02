3c6a63c99db3fab6ad327d6c31dac97218ac0ac2
NekoyashikiShironene <yanason.ys2016@gmail.com>
2025-02-02 20:10:29 +0700

    Fixed function name and modifiers

--- contracts/exercise_1.sol
+++ contracts/exercise_1.sol
@@ -4,19 +4,19 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Operators {
 
-    function add(int256 a, int256 b) public pure returns (int256) {
+    function add(int256 a, int256 b) external pure returns (int256) {
         return a + b;
     }
 
-    function minus(int256 a, int256 b) public pure returns (int256) {
+    function minus(int256 a, int256 b) external pure returns (int256) {
         return a - b;
     }
 
-    function multiply(int256 a, int256 b) public pure returns (int256) {
+    function multiply(int256 a, int256 b) external pure returns (int256) {
         return a * b;
     }
 
-    function devide(int256 a, int256 b) public pure returns (int256) {
+    function divide(int256 a, int256 b) external pure returns (int256) {
         return a / b;
     }
 }
